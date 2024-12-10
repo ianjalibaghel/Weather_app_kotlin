09d4eec404b5af16791ae31dd07d69a8bbfddc34
Anjali Baghel <137780664+ianjalibaghel@users.noreply.github.com>
2024-12-10 16:42:35 +0530

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.weatherapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.weatherapp.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val weatherViewModel= ViewModelProvider(this)[WeatherViewModel::class.java]
+        setContent {
+            WeatherAppTheme {
+               Surface(
+                   modifier= Modifier.fillMaxSize(),
+                   color = MaterialTheme.colorScheme.background
+               ) {
+                   HomeUI(weatherViewModel)
+               }
+            }
+        }
+    }
+}
